b603df675c38751ab99c16d6ebc4fb113d7c1c4b
Tahir Naushasd <t.naushad25@gmail.com>
2017-09-05 09:50:35 +0100

    asp.net core 2.0

--- src/Fiver.Mvc.Testing/Startup.cs
+++ src/Fiver.Mvc.Testing/Startup.cs
@@ -1,7 +1,6 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
 using Fiver.Mvc.Testing.OtherLayers;
 using Microsoft.Extensions.DependencyInjection.Extensions;
 
@@ -20,8 +19,7 @@ namespace Fiver.Mvc.Testing
 
         public void Configure(
             IApplicationBuilder app, 
-            IHostingEnvironment env, 
-            ILoggerFactory loggerFactory)
+            IHostingEnvironment env)
         {
             app.UseDeveloperExceptionPage();
             app.UseMvcWithDefaultRoute();
